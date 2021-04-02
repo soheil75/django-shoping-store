d45e206db6336c858862d8714e9e906ba6720539
soheil75 <soheilabdollahi1375@yahoo.com>
2021-04-02 21:09:21 +0430

    login & logout

--- A/accounts/urls.py
+++ A/accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name="accounts"
 urlpatterns = [
     path('login/',views.user_login,name='login'),
+    path('logout/',views.user_logout,name='logout'),
 ]
--- A/accounts/views.py
+++ A/accounts/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserLoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def user_login(request):
-    return render(request,'accounts/login.html')
+    return render(request, 'accounts/login.html')
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request, email=cd['email'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You are login', 'success')
+                return redirect('shop:home')
+            else:
+                messages.error(
+                    request, 'username or password is not wrong', 'danger')
+                return redirect('shop:home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'you logged out successfully', 'success')
+    return redirect('shop:home')
