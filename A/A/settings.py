aeaa0aef838c400f69280690a15e8786637131d1
soheil75 <soheilabdollahi1375@yahoo.com>
2021-04-02 20:46:51 +0430

    connection to database & add shop app

--- A/A/settings.py
+++ A/A/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'A.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Drama',
+        'USER': 'postgres',
+        'PASSWORD': '369852147',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
